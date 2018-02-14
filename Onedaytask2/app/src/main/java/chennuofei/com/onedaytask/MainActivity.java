a895cf31dc3556625802d1b2a53b936ed892c0ef
xiaoxiaofei <weichatxiaochengxu@163.com>
2018-02-14 21:46:33 +0800

    first commit

---
+++ Onedaytask2/app/src/main/java/chennuofei/com/onedaytask/MainActivity.java
@@ -0,0 +1,13 @@
+package chennuofei.com.onedaytask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
